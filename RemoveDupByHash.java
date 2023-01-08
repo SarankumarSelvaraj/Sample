e50367c1427d75ef883f6652ea71c3796458ee4a
Sarankumar <91582966+SarankumarSelvaraj@users.noreply.github.com>
2023-01-08 11:56:00 +0530

    Update RemoveDupByHash.java

--- RemoveDupByHash.java
+++ RemoveDupByHash.java
@@ -5,7 +5,7 @@ class RemoveDupByHash {
     int[] arr = {2, 5, 7, 1, 2, 2, 2, 5, 5 , 7, 1, 6, 5, 2, 4, 6, 1, 1, 4, 4, 3, 4, 3, 4, 8, 8};
     //Remove the duplicate elements from array using hashmap. create the hashmap.
     HashMap<Integer, Integer> map = new HashMap<>();
-     for(int i = 0; i < arr.length - 1; i++) {
+     for(int i = 0; i < arr.length; i++) {
        if(map.containsKey(arr[i])) {
          map.remove(arr[i]);
        }
